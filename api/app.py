b5a0ff14f97ac8b82f9493a248ff01ff16d3a2ae
AvoMelkonian <melkonianavo7@gmail.com>
2025-01-14 01:37:28 +0300

    NewFix3

--- api/app.py
+++ api/app.py
@@ -5,7 +5,7 @@ from sudoku_generator import generate_sudoku  # Ensure this module generates puz
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route('/solve', methods=['POST'])
 def solve():
